facf451ede076dc424f9a105583248a7fa2df970
Karl Fredrik <kfh@tla.wtf>
2016-09-22 18:13:25 +0200

    retconn

--- caspar_desk/caspar_desk/wsgi.py
+++ caspar_desk/caspar_desk/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for django_admin project.
+WSGI config for caspar_desk project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
